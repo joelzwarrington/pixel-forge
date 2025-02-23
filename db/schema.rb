373807b48a718352c1007caae62db67353572ded
Joel Warrington <joelw@hey.com>
2025-02-22 22:25:02 -0700

    Add location to characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_23_045840) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_23_052029) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "location_id"
+    t.index ["location_id"], name: "index_characters_on_location_id"
     t.index ["name"], name: "index_characters_on_name", unique: true
   end
 
@@ -55,6 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_045840) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "characters", "locations"
   add_foreign_key "equipment_items", "characters"
   add_foreign_key "equipment_items", "items"
   add_foreign_key "inventory_items", "characters"
