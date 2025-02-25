75ecf8f5fb83d0a95ddf94d558f584033a9368e1
Joel Warrington <joelw@hey.com>
2025-02-24 23:21:35 -0700

    Add system to tick actions

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,21 +2,11 @@ class Action < ApplicationRecord
   belongs_to :character
   scope :active, -> { where(stopped_at: nil) }
 
-  before_create :start_ticks
-
   def location
     Location.find(location_id)
   end
 
   def node
     Node.find(node_id)
-  end
-
-  private
-
-  def start_ticks
-    self.started_at = Time.current
-    self.seed = "#{started_at.iso8601}-#{SecureRandom.hex}"
-    self.next_tick_at = started_at + 5.seconds
   end
 end
--- app/models/action_manager.rb
+++ app/models/action_manager.rb
@@ -5,10 +5,58 @@ class ActionManager < ApplicationRecord
     end
 
     def begin(character, node)
-      character.actions.create(location_id: character.location_id, node_id: node.id)
+      Action.transaction do
+        self.end(character)
+        time = Time.current
+
+        character.actions.create(
+          location_id: character.location_id,
+          node_id: node.id,
+          started_at: time,
+          seed: "#{time.iso8601}-#{SecureRandom.hex}",
+          next_tick_at: time + 5.seconds,
+        )
+      end
     end
 
     def end(character)
+      if (action = active_for(character)).present?
+        time = Time.current
+        # executes ticks until stopped_at
+        action.update!(stopped_at: time)
+      end
+    end
+
+    def tick(action)
+      time = Time.current
+      last_tick_at = action.last_tick_at
+      time_since_last_tick = time - last_tick_at
+      seconds_per_tick = 1
+      ticks = (time_since_last_tick / seconds_per_tick).floor
+
+      drops = Hash.new(0)
+
+      ticks.times.each do |tick_count|
+        tick_at = last_tick_at.advance(seconds: seconds_per_tick * tick_count)
+
+        action.node.drops.each do |drop|
+          drops[drop.item.id] += random("#{action.seed}-#{tick_at.iso8601}").rand(drop.minimum_quantity..drop.maximum_quantity)
+        end
+      end
+
+      new_last_tick_at = last_tick_at.advance(seconds: seconds_per_tick * ticks)
+      action.update!(
+        last_tick_at: new_last_tick_at,
+        next_tick_at: new_last_tick_at.advance(seconds: seconds_per_tick)
+      )
+
+      drops
+    end
+
+    def random(seed)
+      bytes = Digest::SHA256.digest seed
+      seed_int = bytes.bytes.inject { |a, b| (a << 8) + b }
+      Random.new(seed_int)
     end
   end
 end
