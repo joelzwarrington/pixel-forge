2e20d3bb519ef0194ff47156e3d15caf66013550
Joel Warrington <joelw@hey.com>
2025-03-02 21:46:12 -0700

    Continue to beautify home UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def show
+    @character = current_character
+    @equipment = @character.equipment
+    @inventory = @character.inventory
+    @location = @character.location
+    @node = @character.actions.active.first&.node
   end
 end
--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,58 +1,21 @@
 class NodesController < ApplicationController
-  before_action :set_node, only: %i[ show edit update destroy ]
+  before_action :set_node, only: %i[ show ]
+  before_action :set_location, only: %i[ show ]
 
-  # GET /nodes
   def index
     @nodes = Node.all
   end
 
-  # GET /nodes/1
   def show
   end
 
-  # GET /nodes/new
-  def new
-    @node = Node.new
-  end
-
-  # GET /nodes/1/edit
-  def edit
-  end
-
-  # POST /nodes
-  def create
-    @node = Node.new(node_params)
-
-    if @node.save
-      redirect_to @node, notice: "Node was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /nodes/1
-  def update
-    if @node.update(node_params)
-      redirect_to @node, notice: "Node was successfully updated.", status: :see_other
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /nodes/1
-  def destroy
-    @node.destroy!
-    redirect_to nodes_path, notice: "Node was successfully destroyed.", status: :see_other
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_node
-      @node = Node.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def node_params
-      params.fetch(:node, {})
-    end
+  def set_node
+    @node = Node.find(params.expect(:id))
+  end
+
+  def set_location
+    @location = Location.find(params.expect(:location_id))
+  end
 end
